8f7098a1d160f67f8ab83e43a5527a2859623a40
Anmol More <amore9@asu.edu>
2022-10-20 22:24:25 -0700

    Implemented ProductMenu

--- DataTypes/MeatProductMenu.java
+++ DataTypes/MeatProductMenu.java
@@ -1,69 +1,41 @@
 package DataTypes;
 
-import javax.swing.*;
-import java.io.*;
+import Utilities.DataReader;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MeatProductMenu extends ProductMenu{
-    int userType;
 
     Map<String, String> meatProducts = new HashMap<>();
 
-    JTable productsList;
-
     public MeatProductMenu(int userType){
         this.userType = userType;
-        initMenu();
+        populateMeatProducts();
+        initMenu(meatProducts);
     }
 
-    @Override
-    public void initMenu() {
-        File userProduct = new File("Database/UserProduct.txt");
-        File buyersInfo = new File("Database/BuyerInfo.txt");
-        File sellersInfo = new File("Database/SellerInfo.txt");
-        BufferedReader buyerBF;
-        BufferedReader sellerBF;
-        BufferedReader productBF;
+    private void populateMeatProducts(){
+        File productInfoFile = new File("Database/ProductInfo.txt");
+        File userProductFile = new File("Database/UserProduct.txt");
+
+        List<String> meats = Arrays.asList(DataReader.readData(productInfoFile).entrySet().stream().filter(e -> e.getKey().equals("Meat"))
+                .map(Map.Entry::getValue).toArray(String[]::new));
 
 
-        String line;
+        if (userType == 0){
+            File sellerInfoFile = new File("Database/SellerInfo.txt");
 
-        try{
-            productBF = new BufferedReader(new FileReader(userProduct));
 
-            if(userType == 0){
-                Map<String, String> sellers = new HashMap<>();
-                sellerBF = new BufferedReader((new FileReader(sellersInfo)));
-                while ((line = sellerBF.readLine()) != null){
-                    sellers.put(line.split(":")[0], line.split(":")[1]);
-                }
-                while ((line = productBF.readLine()) != null){
-                    Map.Entry<String, String> newEntry= Map.entry(line.split(":")[0], line.split(":")[1]);
-                    if (sellers.containsKey(newEntry.getKey())){
-                        meatProducts.put(newEntry.getKey(), newEntry.getValue());
-                    }
-                }
+            filterByUserType(meats, userProductFile, sellerInfoFile);
 
-            } else if (userType == 1) {
-                Map<String, String> buyers = new HashMap<>();
-                buyerBF = new BufferedReader(new FileReader(buyersInfo));
-                while ((line = buyerBF.readLine()) != null){
-                    buyers.put(line.split(":")[0], line.split(":")[1]);
-                }
-            }
+        } else {
+            File buyerInfoFile = new File("Database/BuyerInfo.txt");
 
-            Object[][] productsArray = meatProducts.entrySet().stream().map(e -> new String[]{e.getKey(), e.getValue()})
-                    .toArray(Object[][]::new);
-
-            String[] columnNames= {"Product", "Seller"};
-
-            productsList = new JTable(productsArray, columnNames);
-
-        } catch (IOException e){
-            throw new RuntimeException(e);
+            filterByUserType(meats, userProductFile, buyerInfoFile);
         }
-
     }
 }
--- DataTypes/ProductMenu.java
+++ DataTypes/ProductMenu.java
@@ -1,9 +1,56 @@
 package DataTypes;
 
+import Utilities.DataReader;
+
 import javax.swing.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class ProductMenu extends JDialog {
 
-    public abstract void initMenu();
+    int userType;
+
+    JTable productsTable;
+
+    JPanel mainPanel;
+
+
+    public void initMenu(Map<String, String> productsList){
+        mainPanel = new JPanel();
+
+
+        Object[][] productsArray = productsList.entrySet().stream().map(e -> new String[]{e.getKey(), e.getValue()})
+                .toArray(Object[][]::new);
+
+        String[] columnNames = {"Product", "Seller"};
+
+        productsTable = new JTable(productsArray, columnNames);
+
+        mainPanel.add(productsTable);
+
+        this.add(mainPanel);
+        this.setModal(true);
+        this.setVisible(true);
+
+    }
+
+    protected Map<String, String> filterByUserType(List<String> products, File userProductFile, File userInfoFile) {
+        Map<String, String> result = new HashMap<>();
+
+        List<String> users = Arrays.asList(DataReader.readData(userInfoFile).keySet().toArray(String[]::new));
+        Map<String, String> allProducts;
+        allProducts = DataReader.readData(userProductFile);
+
+        for (Map.Entry<String, String> product:
+                allProducts.entrySet()) {
+            if (users.contains(product.getKey()) && products.contains(product.getValue())){
+                result.put(product.getKey(), product.getValue());
+            }
+        }
+        return result;
+    }
 
 }
